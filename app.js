34f69a6bb437c819aa166ad129a1ae4ec9ff5393
Seba <sebastiandripari@gmail.com>
2020-01-10 19:33:27 -0300

    Creo servidor con express en puerto 3000

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.error("hola mundo")
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
