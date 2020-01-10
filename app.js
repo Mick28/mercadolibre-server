c8d56a7223eab6c9b8605180bd6f7624789602f8
Seba <sebastiandripari@gmail.com>
2020-01-10 19:48:31 -0300

    Agrege endpoint /api/items

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const port = 3000
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+  res.send('Hello World!!')
+})
+
+app.get('/api/items', function (req, res) {
+  res.send('Query!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
